1d52ab47ba94ba0aa80bb7de57ebc3a39c718721
AndyDev2013 <sweeneyandrew2013@gmail.com>
2015-01-01 12:33:42 +0000

    Draggable/Droppable Stuffs Second commit.

--- js/columns.js
+++ js/columns.js
@@ -1,25 +1,71 @@
 function getColumns(){
 
+ var promise = $.ajax(
+ {
+      url: "/some.php",
+      dataType: 'json', 
+      success: function(data) 
+      {
+          var i = 0;
+
+          jQuery.each(data.columns, function(index, item)
+          {
+
+              //alert(JSON.stringify(item));
+              var fitem = $(document.createElement("li")).attr("id", "li"+i);
+              $("#columnsList").append(fitem);
+              //var theItem = document.getElementById('li' + i);
+              //theItem.innerHTML = "Item " + i;
+              $("#li" + i).attr("class", "active ui-widget-content");
+
+              var theLink = $(document.createElement("a")).attr({
+                                                                  href: "index.html",
+                                                                  id: "link" + i
+                                                                });
+              $("#li"+i).append(theLink);
+
+              var theItem = document.getElementById('link'+i);
+              theItem.innerHTML = item.name;
+
+              var theIm = $(document.createElement("i")).attr("class", "fa fa-fw fa-clock-o draggable ui-widget-content");
+              $("#li"+ i + " a").prepend(theIm);
+
+              $(function() 
+              {
+                  //$( "#link" + i ).draggable({ grid: [ 20, 20 ] }); //Working
+                  
+                    $( "#link" + i ).draggable({
+                          revert : function(event, ui) {
+                              // on older version of jQuery use "draggable"
+                              // $(this).data("draggable")
+                              // on 2.x versions of jQuery use "ui-draggable"
+                              // $(this).data("ui-draggable")
+                              $(this).data("uiDraggable").originalPosition = {
+                                  top : 0,
+                                  left : 0
+                              };
+                              // return boolean
+                              return !event;
+                              // that evaluate like this:
+                              // return event !== false ? false : true;
+                          }
+                      });
+                      $("#droppable").droppable();
+                  });
+
+              i++;
+
+          });
+      },
+      error: function(XMLHttpRequest, textStatus, errorThrown)
+      {
+          alert("Badness");
+      }
+  });
+
   for(var i = 0; i < 10; i++)
     {
-      var item = $(document.createElement("li")).attr("id", "li"+i);
-      $("#columnsList").append(item);
-      //var theItem = document.getElementById('li' + i);
-      //theItem.innerHTML = "Item " + i;
-      $("#li" + i).attr("class", "active");
-
-      var theLink = $(document.createElement("a")).attr({
-                                                          href: "index.html",
-                                                          id: "link" + i
-                                                        });
-      $("#li"+i).append(theLink);
-
-      var theItem = document.getElementById('link'+i);
-      theItem.innerHTML = "Column " +i;
-
-      var theIm = $(document.createElement("i")).attr("class", "fa fa-fw fa-clock-o");
-      $("#li"+ i + " a").prepend(theIm);
-
+     
 
     }
 
@@ -54,5 +100,16 @@ function AjaxColumns() {
       >=
       !=
       ==
+      %
+
+      Alternate conditional:
+
+      AND
+      OR
+      FINISHED
+      +
+      /
+      -
+      *
       %
 */
--- some.php
+++ some.php
@@ -1,14 +1,27 @@
 
 <?php
 
-  $data = array( "column" => array( array("name" => "sleepStart", "type" => "TIME", "lowBound" => 0, "highBound" => 24.61),
-                                    array("name" => "sleepEnd", "type" => "TIME", "lowBound" => 0, "highBound" => 24.61),
-                                    array("name" => "CoolingEndTime", "type" => "TIME", "lowBound" => 0, "highBound" => 24.61),
-                                    array("name" => "CookingEndTime", "type" => "TIME", "lowBound" => 0, "highBound" => 24.61),
-                                    array("name" => "StartDate", "type" => "DATE"),
-                                    array("name" => "EndDate", "type" => "DATE")
-                                  )
-  );
+  $columns =  array( array("name" => "sleepStart", "type" => "TIME", "lowBound" => 0, "highBound" => 24.61),
+                  array("name" => "sleepEnd", "type" => "TIME", "lowBound" => 0, "highBound" => 24.61),
+                  array("name" => "CoolingEndTime", "type" => "TIME", "lowBound" => 0, "highBound" => 24.61),
+                  array("name" => "CookingEndTime", "type" => "TIME", "lowBound" => 0, "highBound" => 24.61),
+                  array("name" => "StartDate", "type" => "DATE"),
+                  array("name" => "EndDate", "type" => "DATE")
+                );
+  
+ 
+  $triggers = array("name"=>"row-updated","name"=>"time-elapsed");
+  
+
+  $actions = array(
+    "name"=>"amber_cooling_times_event",
+    "name"=>"amber_sleep_interval_event",
+    "name"=>"red_cooling_times_event",
+    "name"=>"red_sleep_interval_event",
+    "name"=>"red_corretive_action");
+    
+  $data = array("columns"=>$columns, "triggers"=>$triggers, "actions"=>$actions); 
+
   header('Content-Type: application/json');
 
   echo json_encode($data);
